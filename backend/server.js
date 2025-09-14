150bd24aae1906ad4af4cc425ff0f9f525dc9c54
chirag <sharmachirag242004@gmail.com>
2025-09-14 10:28:46 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import app from "./app.js";
+import http from 'http';
+import dotenv from 'dotenv';
+dotenv.config();
+const port = process.env.PORT
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`server is started on ${port}`);
+})
